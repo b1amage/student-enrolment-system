c5c7925783a0e39b7a8c637fec9910ce8be1630c
Quoc Bao Nguyen Luu <quocbaonguyenluu@Quocs-MacBook-Pro.local>
2022-03-18 15:05:40 +0700

    complete csv exception

--- src/exception/WrongCsvFormatException.java
+++ src/exception/WrongCsvFormatException.java
@@ -1,4 +1,12 @@
 package exception;
 
+/**
+ * Class name: WrongCsvFormatException
+ * Role: Exception
+ * Functionality: Custom exception for enhancing user-friendly of the app
+ */
 public class WrongCsvFormatException extends Exception {
+    public WrongCsvFormatException() {
+        super("There is something wrong on the format of your CSV file!");
+    }
 }
