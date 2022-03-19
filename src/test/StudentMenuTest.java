6a58a829934923067c7bf20497ada0c9a6aea626
Quoc Bao Nguyen Luu <quocbaonguyenluu@Quocs-MacBook-Pro.local>
2022-03-19 10:45:58 +0700

    test junit + update class diagram

---
+++ src/test/StudentMenuTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class StudentMenuTest {
+
+    @Before
+    public void setupTest() {
+    }
+
+    @org.junit.Test
+    public void handleViewAllStudent() {
+    }
+
+    @org.junit.Test
+    public void handleViewAllStudentInACourseInASemester() {
+    }
+}
